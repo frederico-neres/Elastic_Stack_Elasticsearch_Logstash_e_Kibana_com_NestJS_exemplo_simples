ce0f97d8ecbb8f739c72d54b5f033f58106a81d3
frederico-neres <neresfrederico@gmail.com>
2023-09-30 19:51:37 -0300

    feat: added domain address

---
+++ src/domain/Address.ts
@@ -0,0 +1,29 @@
+import { getLogger } from "src/configuration/logger";
+import { Logger } from "winston";
+
+const STATES_ALLOWED_FOR_SALE: Array<string> = ['SP', 'MG', 'SC']
+
+export class Address {
+    constructor(
+        private cep: string,
+        private bairro: string,
+        private localidade: string,
+        private uf: string,
+        private logger: Logger
+    ) {
+      this.logger = getLogger();
+    }
+
+    checkPermittedState(): boolean {
+        if(STATES_ALLOWED_FOR_SALE.includes(this.uf)) {
+            this.logger.info(`state ${this.uf} is allowed for sales, 
+            cep: ${this.cep}, bairro: ${this.bairro}, localidade: ${this.localidade}`);
+            return true;
+        };
+
+        this.logger.info(`state ${this.uf} is not allowed for sales,
+         cep: ${this.cep}, bairro: ${this.bairro}, localidade: ${this.localidade}`)
+        return false; 
+    }
+
+}
