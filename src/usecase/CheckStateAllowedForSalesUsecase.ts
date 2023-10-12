12d80e7974386b885cc41974a0b4f7196d257753
frederico-neres <neresfrederico@gmail.com>
2023-10-12 11:50:43 -0300

    feate: use case implemented to check if the state is allowed for sales

---
+++ src/usecase/CheckStateAllowedForSalesUsecase.ts
@@ -0,0 +1,20 @@
+import { Address } from "src/domain/Address";
+import { IAddressExternalClient } from "src/port/IAddressExternalClient";
+import { Logger } from "winston";
+
+export class CheckStateAllowedForSalesUsecase {
+    
+    constructor(
+        private iAddressExternalClient :IAddressExternalClient,
+        private logger: Logger
+    ) {}
+
+    async check(zipCode: string): Promise<boolean> {
+        this.logger.info("[CheckStateAllowedForSalesUsecase] starting checking the state allowed for sales");
+        let address: Address = await this.iAddressExternalClient.getByZipCode(zipCode);
+        let isAllowed: boolean = address.checkPermittedState();
+        this.logger.info("[CheckStateAllowedForSalesUsecase] completed checking the state allowed for sales");
+        return isAllowed;
+    }
+
+}
